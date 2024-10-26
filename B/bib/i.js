7a4d5ff5705c530d0e8a1a3f3afada7fdcfb7724
yoshida09 <2312061@venus.fujijoshi.ac.jp>
2024-10-26 11:33:31 +0900

    Add files via upload

---
+++ B/bib/i.js
@@ -0,0 +1,11 @@
+/*!
+ *
+ *  # bib/i.js (BCK)                                             (℠)
+ *
+ *  * © Satoru Matsushima - https://bibi.epub.link or https://github.com/satorumurmur/bibi
+ *  * Open source under the MIT License - https://github.com/satorumurmur/bibi/blob/master/LICENSE
+ *
+ *  * Calling:
+ *      - Jo | Helper for Embedding Bibi-Frames in Webpage. - bibi/and/jo.js
+ *
+ */!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=46)}({46:function(e,t){var r,n,o;window["bibi:jo"]||(r=document.currentScript||(o=document.getElementsByTagName("script"))[o.length-1])&&r.src&&/\/bib\/i\.js$/.test(r.src)&&((n=document.createElement("script")).src=r.getAttribute("src").replace(/bib\/i\.js$/,"bibi/and/jo.js"),n.onload=function(){return(e=window["bibi:jo"]).Status||e.embed();var e},document.head.appendChild(n))}});
